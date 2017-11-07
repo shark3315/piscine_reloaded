a16b9b4cb921720a36957bc29da6498dbe31bd8b
Loulou <Loulou@imac-de-laetitia.home>
2017-11-07 15:51:16 +0100

    Add ex15

---
+++ ex15/ft_putstr.c
@@ -0,0 +1,19 @@
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int main (void)
+{
+	ft_putstr("test");
+	return 0;
+}
